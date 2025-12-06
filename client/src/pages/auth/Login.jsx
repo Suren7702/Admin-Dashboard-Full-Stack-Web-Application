bffbed2f56c929adb82246b6e548ad91b95c5f78
Surendhar <suren7702@gmail.com>
2025-12-06 09:32:37 +0530

    login page changed v1

--- client/src/pages/auth/Login.jsx
+++ client/src/pages/auth/Login.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "http://localhost:5000";
+  import.meta.env.VITE_API_URL || "http://localhost:5000";
 
 export default function Login() {
   const navigate = useNavigate();
